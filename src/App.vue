bc0f86cb8803df84b43f5c9be7ebf5181ba9a816
Sebastian Spreizer <spreizer@web.de>
2023-10-28 16:57:44 +0200

    Add profile picture

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Profile" class="logo" src="./assets/img/spreizer.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
